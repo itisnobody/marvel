75fbf4ddb61bf41e2fb025426777417db1e6fae6
Sasha <alexander.goryachev@gmail.com>
2022-05-24 19:31:46 +0300

    char info

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -4,7 +4,6 @@ import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
 
 class CharList extends Component {
     state = {
@@ -45,7 +44,8 @@ class CharList extends Component {
             return (
                 <li 
                     className="char__item"
-                    key={item.id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
