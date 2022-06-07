1687d86b8b15a68630be44364c7a5b5f442c52a5
Sasha <alexander.goryachev@gmail.com>
2022-06-07 18:31:36 +0300

    use react.lazy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,10 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { MainPage, ComicsPage, ComicPage, Page404 } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from "../spinner/Spinner";
+import { MainPage, ComicsPage, ComicPage } from "../pages";
+const Page404 = lazy(() => import('../pages/404'));
 
 const App = () => {
     return (
@@ -9,12 +12,14 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path='/' element={<MainPage/>}/>
-                        <Route path='/comics' element={<ComicsPage/>}/>
-                        <Route path='/comics/:comicId' element={<ComicPage/>}/>
-                        <Route path="*" element={<Page404/>}/>
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path='/' element={<MainPage/>}/>
+                            <Route path='/comics' element={<ComicsPage/>}/>
+                            <Route path='/comics/:comicId' element={<ComicPage/>}/>
+                            <Route path="*" element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
